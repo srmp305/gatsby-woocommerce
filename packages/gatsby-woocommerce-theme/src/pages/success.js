65fe0f509e06c2573f02500feb07aeff4f690404
= <=>
2021-02-05 13:45:44 +0530

    succes and cancel page design

--- packages/gatsby-woocommerce-theme/src/pages/success.js
+++ packages/gatsby-woocommerce-theme/src/pages/success.js
@@ -1,12 +1,17 @@
 import React from "react"
 import Layout from "../components/layout";
+import success from "../images/success-icon.svg";
 const PaymentSuccessPage = ()=>{
     
     return(
          <Layout>
-        <div className="container">
-        Payment completed.
-        </div>
+        <div className="message-content">
+          <div className="messagae-box">
+                <img src={success} alt="" />
+                <h2>Payment Completed</h2>
+                <p>Thank you your payment has been completed.</p>
+          </div>
+          </div>
          </Layout>
     )
 }
