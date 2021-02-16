8b5721ee5776db9ee1fc1685b3d34cb9c9893401
ashrafwbst <er.ashrafkamal@gmail.com>
2021-02-16 12:46:41 +0530

    comment instafeed

--- packages/gatsby-woocommerce-theme/src/components/instafeed/instafeed.js
+++ packages/gatsby-woocommerce-theme/src/components/instafeed/instafeed.js
@@ -3,29 +3,30 @@ import { graphql, StaticQuery } from 'gatsby';
 
 export default () => {
   return (
-    <StaticQuery
-      query={graphql`
-        query InstaFeed {
-          allInstagramContent(limit: 6) {
-            nodes {
-              media_id
-              media_url
-              id
-              media_type
-            }
-          }
-        }
-      `}
-      render={(data) => {
-        const feed = data.allInstagramContent.nodes;
-        return (
-          <>
-            {feed.map((el) => {
-              return <img src={el.media_url} alt="/" />;
-            })}
-          </>
-        );
-      }}
-    />
+    // <StaticQuery
+    //   query={graphql`
+    //     query InstaFeed {
+    //       allInstagramContent(limit: 6) {
+    //         nodes {
+    //           media_id
+    //           media_url
+    //           id
+    //           media_type
+    //         }
+    //       }
+    //     }
+    //   `}
+    //   render={(data) => {
+    //     const feed = data.allInstagramContent.nodes;
+    //     return (
+    //       <>
+    //         {feed.map((el) => {
+    //           return <img src={el.media_url} alt="/" />;
+    //         })}
+    //       </>
+    //     );
+    //   }}
+    // />
+    <></>
   );
 };
