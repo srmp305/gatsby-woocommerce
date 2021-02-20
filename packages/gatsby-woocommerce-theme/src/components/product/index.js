f5fc0a5ccd2f5a32107d238d1a98b04641871a1f
ashrafwbst <er.ashrafkamal@gmail.com>
2021-02-20 18:51:28 +0530

    short desc issue fix

--- packages/gatsby-woocommerce-theme/src/components/product/index.js
+++ packages/gatsby-woocommerce-theme/src/components/product/index.js
@@ -54,7 +54,7 @@ const Product = (props) => {
         </Link>
         <div className="card-body text-center">
           <h3 className="card-header">{product.name ? product.name : ""}</h3>
-          <p dangerouslySetInnerHTML={{__html: product.shortDescription.replace(/(<? *script)/gi, 'illegalscript')}}></p>
+          <p dangerouslySetInnerHTML={{__html: product.shortDescription}}></p>
           <h6 className="card-subtitle">{product.price}</h6>
           
           <AddToCartButton product={product} />
